6160cfbd10eca28aa936a675480071552016a8ce
ogerly <ogerly@tutanota.com>
2025-03-06 09:33:16 +0100

    check gh-pags

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/dein-repository-name/', // Wichtig für GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
