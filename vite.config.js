2a56fa14fdfa6d0076fa97aa1b2ec04757430247
ogerly <ogerly@tutanota.com>
2025-03-06 10:01:24 +0100

    gh-pages deplay check

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/dm-pre-alpha/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
